9d7a620680859a8ebcecd70d7930f6d8f32bf5eb
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-09-23 13:15:37 +1000

    Add docstrings to password check functions for clarity

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,11 +3,13 @@ def main():
     print_stars_of_length(password)
 
 
-def print_stars_of_length(password):
-    print("*" * len(password))
+def print_stars_of_length(length):
+    """Print a number of asterisks/stars on one line"""
+    print("*" * len(length))
 
 
 def get_password():
+    """Prompt user for a password 5 or more characters in length"""
     password = input("Password: ")
     while len(password) < 5:
         print("Password is too small, must be 5 or more characters")
