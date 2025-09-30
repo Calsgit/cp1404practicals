298b1adf344b957a5fea8e4d26c774278539d236
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-09-30 13:32:01 +1000

    Complete second question

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,10 +1,14 @@
 # 1. Write code that asks the user for their name, then opens a file called name.txt and writes that name to it.
 name = input("Name: ")
-infile = open("name.txt", 'w')
-infile.write(name)
-infile.close()
+outfile = open("name.txt", 'w')
+outfile.write(name)
+outfile.close()
 
 # 2. In the same file, write code that opens name.txt and reads the same then prints
+infile = open("name.txt", 'r')
+name = infile.readline()
+print(name)
+infile.close()
 
 # 3. Create a text file called numbers.txt and save it in your directory. Put the following three numbers on separate lines in the file and save it:
 # 17, 42, 400
