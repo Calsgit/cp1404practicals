a6428bedb3e3ae115777c064ac2baa994f275bc2
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-09-30 12:29:44 +1000

    Complete first task, counting days

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -17,6 +17,7 @@ INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
 print(f"${price:,.2f}")
+number_of_days = 1
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -30,6 +31,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
-
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    number_of_days += 1
