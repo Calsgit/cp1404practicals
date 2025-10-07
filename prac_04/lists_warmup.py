06cf00ed62e5cedb685326ac69413999610b9513
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-10-07 12:10:46 +1000

    Add  additional statements

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -8,3 +8,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers = false
 # "3" in numbers = false
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
