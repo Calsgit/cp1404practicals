58b6c339b429cbf9915f017f38cc8bec5dc95e41
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-10-14 12:42:04 +1000

    Add main and count_words_in_string functions

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,11 @@
+def main():
+    sentence = input("Text: ")
+    count_words_in_string(sentence)
+
+
+def count_words_in_string(text):
+    """Calculate the amount of times each word is used in a given string"""
+    pass
+
+
+main()
