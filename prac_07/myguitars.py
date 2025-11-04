3cd32076773d9e362386849acc3ea936ab6a2136
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-04 13:26:58 +1000

    Add function to save guitars to file Add get_guitars_from_user function to main()

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,24 +4,30 @@ Elapsed time: TODO: Elapsed time
 """
 from guitar import Guitar
 
-IN_FILE = "guitars.csv"
+GUITARS_FILE = "guitars.csv"
 
 
 def main():
     print("My guitars!")
-    guitars = get_guitars_from_file().sort()
+    guitars = get_guitars_from_file()
+    new_guitars = get_guitars_from_user()
+    if new_guitars:
+        guitars += new_guitars
+    guitars.sort()
     display_guitars(guitars)
+    save_guitars(guitars)
 
 
 def get_guitars_from_file():
-    """Open IN_FILE and extract csv data to create a list of guitar objects."""
+    """Open specified file and extract csv data to create a list of guitar objects."""
     guitars = []
-    with open(IN_FILE) as in_file:
+    with open(GUITARS_FILE) as in_file:
         in_file.readline()
         for line in in_file:
             parts = line.strip().split(',')
             guitars.append(Guitar(parts[0], int(parts[1]), float(parts[2])))
     return guitars
+
 
 def get_guitars_from_user():
     """Repeatedly prompts user for guitar information until a blank name is entered."""
@@ -34,19 +40,26 @@ def get_guitars_from_user():
         guitars.append(guitar)
         print(guitar, "added.")
         name = input("Name: ")
-    print()
     return guitars
 
 
 def display_guitars(guitars):
     """Loop through a list of guitars and display their information."""
-    max_name_length = max([len(guitar.name) for guitar in guitars])
-    max_cost_length = max([len(str(guitar.cost)) for guitar in guitars])
+    max_name_length = max(len(guitar.name) for guitar in guitars)
+    max_cost_length = max(len(str(guitar.cost)) for guitar in guitars)
     print("These are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = "(vintage)" if guitar.is_vintage() else ""
         print(
             f"Guitar {i}: {guitar.name:>{max_name_length}} ({guitar.year:>4}), worth $ {guitar.cost:>{max_cost_length},.2f} {vintage_string}")
+
+
+def save_guitars(guitars):
+    """Save all stored guitars to the specified file."""
+    with open(GUITARS_FILE, 'w') as out_file:
+        for guitar in guitars:
+            guitar_string = f"{guitar.name},{guitar.year},{guitar.cost}\n"
+            out_file.write(guitar_string)
 
 
 main()
