ab6699f59204def1123c3c6d9e1092e4437c7645
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-04 14:10:06 +1000

    Add remaining functions as templates

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,12 +7,23 @@ import datetime
 from project import Project
 
 DEFAULT_FILE = "projects.txt"
+MENU_OPTIONS = """- (L)oad projects
+- (S)ave projects
+- (D)isplay projects
+- (F)ilter projects by date
+- (A)dd new project
+- (U)pdate project
+- (Q)uit"""
 
 
 def main():
     print("Welcome to Pythonic Project Management")
     projects = load_projects(True)
-    print(projects)
+    print(MENU_OPTIONS)
+    selection = input(">>> ").lower()
+    while selection != 'q':
+        if selection == 's':
+            save_projects(projects)
 
 
 def load_projects(is_on_startup=False):
@@ -27,6 +38,29 @@ def load_projects(is_on_startup=False):
                 Project(parts[0], datetime.datetime.strptime(parts[1], "%d/%m/%Y").date(), int(parts[2]),
                         float(parts[3]), int(parts[4])))
     return projects
+
+
+def save_projects(projects, preferred_file=""):
+    if preferred_file == "":
+        preferred_file = input(f"Would you like to save to {DEFAULT_FILE}? ").lower()
+    if preferred_file == "y" or "yes":
+        pass  # TODO: saving
+
+
+def display_projects(projects):
+    pass  # TODO: display
+
+
+def filter_projects(projects):
+    pass  # TODO: filter by after input date
+
+
+def add_project():
+    pass  # TODO: add new project
+
+
+def update_project(project):
+    pass  # TODO: update selected project
 
 
 if __name__ == '__main__':
