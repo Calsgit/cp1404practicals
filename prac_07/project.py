7311f12bb129065a85f16f6f825b4cec56a43713
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-04 13:57:18 +1000

    Add method to represent object as string

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,3 +8,7 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __repr__(self):
+        """Represent class as a string."""
+        return f"Project {self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:.,2f}, completion: {self.completion_percentage}%"
