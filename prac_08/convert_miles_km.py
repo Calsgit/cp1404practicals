390258e0cc37d8db84881f26db37324658dcabe7
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-11 13:22:29 +1000

    Figure out after 30 minutes you need to put brackets after the method names

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,10 +6,12 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+MILES_TO_KM_RATIO = 1.60934
+
 
 class Convert_Miles_Km(App):
     """Convert_Miles_Km is an app for converting a value in miles to kilometres."""
-    output = StringProperty() # I don't understand this
+    output = StringProperty()  # I don't understand this
 
     def build(self):
         """Build the kivy app from the corresponding kv file."""
@@ -17,5 +19,21 @@ class Convert_Miles_Km(App):
         self.output = '0'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_up(self):
+        try:
+            self.root.ids.input_text.text = str(float(self.root.ids.input_text.text) + 1)
+        except ValueError:
+            self.root.ids.input_text.text = '0'
+
+    def handle_down(self):
+        try:
+            self.root.ids.input_text.text = str(float(self.root.ids.input_text.text) - 1)
+        except ValueError:
+            self.root.ids.input_text.text = '0'
+
+    def handle_convert(self):
+        self.output = str(float(self.root.ids.input_text.text) * MILES_TO_KM_RATIO)
+
 
 Convert_Miles_Km().run()
