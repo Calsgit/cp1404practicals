a0f59399d0da47aa233fa1314525d0a0d7623b39
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-11 13:35:17 +1000

    Add dynamic_labels files and class build method

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,22 @@
+"""Dynamic labels
+Time estimate: 20 min
+Time elapsed:
+"""
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+NAMES = ("me", "you", "them", "him", "her", "the guy on the side of the road")
+
+
+class dynamic_labels(App):
+    """Dynamic labels is a simple kivy app used for testing adding dynamic labels to the UI."""
+
+    def build(self):
+        """Build the kivy app from the corresponding kv file."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        return self.root
+
+
+dynamic_labels().run()
