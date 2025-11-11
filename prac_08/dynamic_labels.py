6beda5097a8eefce055c3ce65767c1282f4941c4
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-11 13:52:05 +1000

    Attempt (and fail) to fix code

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -4,10 +4,9 @@ Time elapsed:
 """
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.uix.label import Label
 
 NAMES = ("me", "you", "them", "him", "her", "the guy on the side of the road")
-
 
 class dynamic_labels(App):
     """Dynamic labels is a simple kivy app used for testing adding dynamic labels to the UI."""
@@ -16,7 +15,14 @@ class dynamic_labels(App):
         """Build the kivy app from the corresponding kv file."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
         return self.root
+
+    def create_labels(self):
+        for name in NAMES:
+            label = Label(text=name)
+            self.root.ids.main.add_widget(label)
+
 
 
 dynamic_labels().run()
