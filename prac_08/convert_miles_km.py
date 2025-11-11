eac947b9e95e0c96beded01f4ded53395f6e8572
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-11 12:59:23 +1000

    Add kivy layout, figure out how stringproperty works

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,12 +4,18 @@ Time elapsed:
 """
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.properties import StringProperty
+
 
 class Convert_Miles_Km(App):
     """Convert_Miles_Km is an app for converting a value in miles to kilometres."""
+    output = StringProperty() # I don't understand this
+
     def build(self):
         """Build the kivy app from the corresponding kv file."""
         self.title = "Convert miles to kilometres"
+        self.output = '0'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+Convert_Miles_Km().run()
