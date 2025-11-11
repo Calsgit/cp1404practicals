f3a122a84cf9ecec3c8761d02361a50c9bb342cf
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-11 12:25:19 +1000

    Complete second half of related instructions

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,6 +9,10 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print('greet')
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = "Enter your name"
+        self.root.ids.input_name.text = ''
 
 BoxLayoutDemo().run()
