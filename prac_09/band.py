c81976beb1376a80d468af5a8d713a205454a75b
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-18 13:22:08 +1000

    Add band, band class and init method

---
+++ prac_09/band.py
@@ -0,0 +1,7 @@
+class Band:
+    """Band class"""
+
+    def __init__(self, name=""):
+        """Construct a band with name and empty member list."""
+        self.name = name
+        self.members = []
