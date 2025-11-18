23efc7d90628b464a7027e44e29c8939ca1476bb
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-18 13:48:45 +1000

    Add imports and add main function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,5 +1,22 @@
 """
-Time estimate: forever
+Time estimate: 30 min
 Time elapsed:
 """
 
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    """Display various menu options for user to interact with to drive their virtual taxis."""
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
+    print("Let's drive!")
+    print(MENU)
+    selection = input(">>> ")
+
+
+main()
