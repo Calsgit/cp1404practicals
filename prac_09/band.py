51c539d139f6e73075ccde33a956eac7a42e293a
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-18 13:38:56 +1000

    Add str method to band

--- prac_09/band.py
+++ prac_09/band.py
@@ -6,6 +6,12 @@ class Band:
         self.name = name
         self.members = []
 
+    def __str__(self):
+        members_string = ""
+        for member in self.members:
+            members_string += f"{member}, "
+        return f"{self.name} ({members_string})"
+
     def add(self, member):
         """Add a member to the band's member list."""
         self.members.append(member)
