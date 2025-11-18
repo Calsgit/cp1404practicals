991c6d111a2cca5ed3ce485071b21597896c30b4
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-18 12:33:52 +1000

    Add drive method to unreliable_car class

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,4 +1,5 @@
 from prac_09.car import Car
+from random import randint
 
 
 class Unreliable_Car(Car):
@@ -8,3 +9,10 @@ class Unreliable_Car(Car):
         """Initialise an unreliable car instance based on the Car class."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive like parent car but only if a random integer is less than the reliability."""
+        distance_driven = 0
+        if randint(0, 100) < self.reliability:
+            distance_driven = super().drive(distance)
+        return distance_driven
