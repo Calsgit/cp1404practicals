f2885087e79b064729c6ec139d12590be65fb9df
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-18 13:24:21 +1000

    Add add method to band

--- prac_09/band.py
+++ prac_09/band.py
@@ -5,3 +5,7 @@ class Band:
         """Construct a band with name and empty member list."""
         self.name = name
         self.members = []
+
+    def add(self, member):
+        """Add a member to the band's member list."""
+        self.members.append(member)
